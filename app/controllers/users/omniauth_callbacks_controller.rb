373619cae849638cdffda02ca0e8affad8be818f
heraldohuerta <heraldo.huerta@gmail.com>
2018-04-07 21:40:33 -0300

    ajuste facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -32,7 +32,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
   # Debemos crear el método .from_omniauth en nuestro modelo User (app/models/user.rb)
       @user = User.from_omniauth(request.env["omniauth.auth"])
-      byebug
       if @user.persisted?
         sign_in_and_redirect @user
         set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
